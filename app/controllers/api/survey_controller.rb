affc3fabe0e790cbc4e5ed237b17d015b3e46557
Hamish Cundy <hamishcundy@gmail.com>
2015-08-16 17:56:27 +1200

    Collection sources on GetSurveyDetails

--- app/controllers/api/survey_controller.rb
+++ app/controllers/api/survey_controller.rb
@@ -2,7 +2,7 @@ module Api
   class SurveyController < ApplicationController
 
     def survey_details
-      render json: Survey.first.as_json(:only => [:name, :terms, :details_required])
+      render json: Survey.first.as_json(:only => [:name, :terms, :details_required], :include=> {:collection_sources => {only:[:key, :required]}})
       
     end
 
