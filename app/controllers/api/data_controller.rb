963cdfe60fa84fda6eff570323597dafc4470dac
Hamish Cundy <hamishcundy@gmail.com>
2015-08-20 21:15:45 +1200

    Metadata write setup

--- app/controllers/api/data_controller.rb
+++ app/controllers/api/data_controller.rb
@@ -1,19 +1,24 @@
 module Api
   class DataController < ApplicationController
 
+    skip_before_filter  :verify_authenticity_token #since its an api controller, dont need csrf protection
+
+
     def upload
-      
+      logger.debug params
       @participant = Participant.find(params[:participantId])
       @key = params[:source]
-      case @key
-      when "call_logs"
+      params[:payload].each do|p|
+        case @key
+        when "call_logs"
 
-      when "sms_logs"
+        when "sms_logs"
 
-      when "contacts"
+        when "contacts"
 
-      when "installed_apps"
-        
+        when "installed_apps"
+
+        end
       end
       
     end
