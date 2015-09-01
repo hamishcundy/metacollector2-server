11a452653ce242676008fa9e136eddf6bffd2f98
Hamish Cundy <hamishcundy@gmail.com>
2015-09-01 22:05:46 +1200

    Fixed call log relationship

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -8,6 +8,8 @@ class GraphController < ApplicationController
 
   def timeline_data
     participant = Participant.first
+    dates = participant.sms_log_records.pluck(:date)
+    logger.debug dates
     respond_to do |format|
       format.json {
         render :json => [1,2,3,4,5]
