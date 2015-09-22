79cfb7411bee8ce3ab2990e6b316ac9b9b66659d
Hamish Cundy <hamishcundy@gmail.com>
2015-09-22 18:45:29 +1200

    Facebook ui setup

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,4 +4,5 @@ class Participant < ActiveRecord::Base
   has_many :installed_app_records
   has_many :location_records
   has_many :conversations, foreign_key: "participant_id", class_name: "FacebookConversation"
+  has_many :messages, through: :conversations
 end
